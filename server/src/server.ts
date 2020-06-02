e083ede486506c9980fde90b7b2bfb3eac0d7cf0
Kleiton Marques <kleitonm.marques@gmail.com>
2020-06-02 11:45:58 -0300

    Conceitos --- Tipos de parametros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,38 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
+app.use(express.json())
+
+const users = [
+    'Diego',
+    'Kleiton',
+    'Robson',
+    'Daniel',
+    'Maria'
+]
 
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuarios')
+    const search = String(request.query.search)
 
-    response.json([
-        'Diego',
-        'Kleiton',
-        'Robson',
-        'Daniel',
-        'Maria'
-    ])
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
+
+    return response.json(filteredUsers)
+})
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const user = users[id]
+    return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+    const data = request.body
+    console.log(data)
+    const user = {
+        name: data.name,
+        email: data.email
+    }
+    return response.json(user)
 })
 
 app.listen(3333)
