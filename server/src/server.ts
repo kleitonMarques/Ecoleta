db156e406a53ff718547cbf1208e636fcf7d9838
Kleiton Marques <kleitonm.marques@gmail.com>
2020-06-02 12:02:48 -0300

    Separando o Server das Rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,8 @@
 import express, { request, response } from 'express'
+import routes from './routes'
 
 const app = express()
 app.use(express.json())
-
-const users = [
-    'Diego',
-    'Kleiton',
-    'Robson',
-    'Daniel',
-    'Maria'
-]
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search)
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
-
-    return response.json(filteredUsers)
-})
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const user = users[id]
-    return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-    const data = request.body
-    console.log(data)
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-    return response.json(user)
-})
+app.use(routes)
 
 app.listen(3333)
