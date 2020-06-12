3b8d17fd81243c64cf335e985c06ce96b32ad608
Kleiton Marques <kleitonm.marques@gmail.com>
2020-06-11 21:42:25 -0300

    Carregando a localidade do usuario no mapa

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -29,9 +29,19 @@ const CreatePoint = () => {
     const [ufs, setUfs] = useState<string[]>([])
     const [cities, setCities] = useState<string[]>([])
 
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
+
     const [selectedUf, setSelectedUf] = useState('0')
     const [selectedCity, setSelectedCity] = useState('0')
     const [selectPosition, setSelectPosition] = useState<[number, number]>([0, 0])
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords
+
+            setInitialPosition([latitude, longitude])
+        })
+    }, [])
 
     useEffect(() => {
         api.get('/items').then(response => {
@@ -111,7 +121,7 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
-                        <Map center={[-23.5118995, -46.9522794]} zoom={15} onClick={handleMapClick}>
+                        <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
                             <TileLayer
                                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
